99750ad632dda967a486c2c38886723f2fe1c586
liming.cao <liming.cao@rokid.com>
2023-01-04 21:10:44 +0800

    添加点光源

--- src/views/learn-three/lib/lights.ts
+++ src/views/learn-three/lib/lights.ts
@@ -2,9 +2,9 @@ import { AmbientLight, Object3D, PointLight } from "three";
 
 
 const ambientLight:AmbientLight = new AmbientLight('rgb(255,255,255)',0.3)
-const pointLight:PointLight = new PointLight('rgb(255,255,255)',0.7)
+const pointLight:PointLight = new PointLight('rgb(255,255,255)',0.7,200,0.1)
 pointLight.position.set(100,100,100)
 
 export const LightsList:Object3D[] = []
 
-LightsList.push(pointLight)
+LightsList.push(ambientLight,pointLight)
