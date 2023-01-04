c5abad7c20a864d42964090399708ea1004e1c05
liming.cao <liming.cao@rokid.com>
2023-01-04 15:57:56 +0800

    learn:丰富场景物体

--- src/views/learn-three/index.tsx
+++ src/views/learn-three/index.tsx
@@ -1,5 +1,6 @@
 import React,{ useRef,useEffect } from 'react'
 import Engine from './lib/engine'
+import {BasicObjectList} from './lib/basic-object'
 import s from './index.module.less'
 
 interface LearnThreeProps {}
@@ -8,6 +9,8 @@ const LearnThree: React.FC<LearnThreeProps> = componentProps => {
     const containerRef = useRef<HTMLDivElement>(null)
     useEffect(() => {
       const engine = new Engine(containerRef.current!)
+     
+      engine.addObject(...BasicObjectList)
     }, [])
     
     return <div className={s.root} ref={containerRef}>
--- src/views/learn-three/lib/engine.ts
+++ src/views/learn-three/lib/engine.ts
@@ -1,5 +1,6 @@
-import {AmbientLight, AxesHelper, BoxGeometry, GridHelper, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer} from 'three'
+import {AmbientLight, AxesHelper, BoxGeometry, GridHelper, Mesh, MeshStandardMaterial, MOUSE, Object3D, PerspectiveCamera, Scene, Vector3, WebGLRenderer} from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 class Engine {
 
@@ -14,57 +15,73 @@ class Engine {
    */
   constructor(dom:HTMLDivElement) {
 
-    const renderer = new WebGLRenderer()
+    const renderer = new WebGLRenderer({antialias:true})
     renderer.setSize(dom.offsetWidth,dom.offsetHeight,true)
 
-    const camera = new PerspectiveCamera(45,dom.offsetWidth/dom.offsetHeight)
-    camera.position.set(200,200,200)
+    const camera = new PerspectiveCamera(45,dom.offsetWidth/dom.offsetHeight,1,1000)
+    camera.position.set(20,20,20)
     camera.lookAt(new Vector3(0,0,0))
     camera.up = new Vector3(0,1,0)
-
-    const orbitControls:OrbitControls  = new OrbitControls(camera,renderer.domElement)
    
-    const box: Mesh = new Mesh(
-      new BoxGeometry(50,50,50),
-      new MeshStandardMaterial({
-        color:'rgb(255,0,0)'
-      })
-    )
+   
 
     const scene = new Scene()
  
     const ambientLight:AmbientLight = new AmbientLight('rgb(255,255,255)',1)
 
-    const axeshelper:AxesHelper  = new AxesHelper(400)
-    const gridHelper:GridHelper  = new GridHelper(400,30)
+    const axeshelper:AxesHelper  = new AxesHelper(500)
+    const gridHelper:GridHelper  = new GridHelper(500,20,'rgb(200,200,200)','rgb(100,100,100)')
+
+
+    scene.add(ambientLight)
+    scene.add(axeshelper)
+    scene.add(gridHelper)
+
+    const stats:Stats = Stats()
+    const statsDom = stats.domElement
+    statsDom.style.position = 'fixed'
+    statsDom.style.top = '0'
+    statsDom.style.right = '5px'
+    statsDom.style.left = 'unset'
   
 
     // this.renderer.setClearColor("rgb(255,255,255)")
     // this.renderer.clearColor()
 
-    scene.add(box)
-    scene.add(ambientLight)
-    scene.add(axeshelper)
-    scene.add(gridHelper)
 
+    const orbitControls:OrbitControls  = new OrbitControls(camera,renderer.domElement)
+    // orbitControls.autoRotate = true
+    // orbitControls.autoRotateSpeed = 0.3
+    // orbitControls.enableDamping = true
+    orbitControls.mouseButtons = {
+      LEFT:null as unknown as MOUSE,
+      MIDDLE:MOUSE.DOLLY,
+      RIGHT:MOUSE.ROTATE
+    }
 
     const renderFunc = ()=>{
-      box.position.x+=-0.1
-      box.position.y+=0.1
-      camera.position.x+=-0.1
+      orbitControls.update()
       renderer.render(scene,camera)
+      stats.update()
       requestAnimationFrame(renderFunc)
     }
 
     renderFunc()
 
     dom.appendChild(renderer.domElement)
+    dom.appendChild(statsDom)
 
     this.dom = dom
     this.renderer = renderer
     this.scene = scene
     this.camera = camera
     this.orbitControls = orbitControls
+  }
+
+  addObject(...object:Object3D[]){
+    object.forEach(elem=>{
+      this.scene.add(elem)
+    })
   }
 }
 
