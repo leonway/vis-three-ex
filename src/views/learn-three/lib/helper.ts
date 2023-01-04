a235bc7feca4cb610ec22d490dc61b8049a97d3f
liming.cao <liming.cao@rokid.com>
2023-01-04 21:55:03 +0800

    添加平行光、聚光灯、半球光以及他们的辅助

--- src/views/learn-three/lib/helper.ts
+++ src/views/learn-three/lib/helper.ts
@@ -1,11 +1,22 @@
-import { AxesHelper, GridHelper, Object3D, PointLightHelper } from "three"
-import {pointLight} from './lights'
+import { AxesHelper, DirectionalLightHelper, GridHelper, HemisphereLightHelper, Object3D, PointLightHelper,SpotLightHelper } from "three"
+import {pointLight,spotLight,directionalLight,hemisphereLight} from './lights'
 
 const axeshelper:AxesHelper  = new AxesHelper(500)
 const gridHelper:GridHelper  = new GridHelper(500,20,'rgb(200,200,200)','rgb(100,100,100)')
 
 const pointLightHelper:PointLightHelper = new PointLightHelper(pointLight,pointLight.distance,pointLight.color)
 
+const spotLightHelper = new SpotLightHelper( spotLight );
+const directionalLightHelper = new DirectionalLightHelper( directionalLight,5 );
+const hemisphereLightHelper = new HemisphereLightHelper( hemisphereLight,5);
+
 export const HelperList:Object3D[] =[]
 
-HelperList.push(axeshelper,gridHelper,pointLightHelper)
+HelperList.push(
+  axeshelper,
+  gridHelper,
+  pointLightHelper,
+  spotLightHelper,
+  directionalLightHelper,
+  hemisphereLightHelper
+  )
--- src/views/learn-three/lib/lights.ts
+++ src/views/learn-three/lib/lights.ts
@@ -1,9 +1,26 @@
-import { AmbientLight, Object3D, PointLight } from "three";
+import { AmbientLight, DirectionalLight, HemisphereLight, Object3D, PointLight, SpotLight, Vector3 } from "three";
 
 const ambientLight:AmbientLight = new AmbientLight('rgb(255,255,255)',0.3)
-export const pointLight:PointLight = new PointLight('rgb(255,255,255)',0.7,200,0.1)
-pointLight.position.set(100,100,100)
+
+export const directionalLight:DirectionalLight = new DirectionalLight('rgb(255,255,255)',1)
+directionalLight.lookAt(new Vector3(20,20,20))
+directionalLight.position.set(100,100,100)
+
+export const spotLight:SpotLight = new SpotLight('rgb(255,255,255)',1)
+spotLight.position.set( 20, 20, 20 );
+
+export const hemisphereLight:HemisphereLight = new HemisphereLight('rgb(0,255,255)','rgb(255,0,255)',5)
+hemisphereLight.position.set(-20,10,0)
+
+export const pointLight:PointLight = new PointLight('rgb(255,255,255)',0.7,50,0.1)
+pointLight.position.set(60,20,-20)
 
 export const LightsList:Object3D[] = []
 
-LightsList.push(ambientLight,pointLight)
+LightsList.push(
+  ambientLight,
+  spotLight,
+  pointLight,
+  directionalLight,
+  hemisphereLight
+  )
