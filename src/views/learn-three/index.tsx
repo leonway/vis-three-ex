75f93f8854941b1b1399dd5b6a1de80cf54f2701
liming.cao <liming.cao@rokid.com>
2023-01-06 14:09:52 +0800

    外部模型加载优化

--- src/views/learn-three/index.tsx
+++ src/views/learn-three/index.tsx
@@ -4,7 +4,7 @@ import {BasicObjectList} from './lib/basic-object'
 import {LightsList} from './lib/lights'
 import {HelperList} from './lib/helper'
 import {CodeModelList} from './lib/code-model'
-import {framePromise,frameMaterial} from './lib/load-model'
+import {framePromise,getFrame} from './lib/load-model'
 import s from './index.module.less'
 import { Material, Mesh } from 'three'
 
@@ -19,20 +19,8 @@ const LearnThree: React.FC<LearnThreeProps> = componentProps => {
       engine.addObject(...LightsList)
       engine.addObject(...HelperList)
       engine.addObject(...CodeModelList)
-      framePromise.then(group=>{
-        console.log('group',group);
-      const scene = group.scenes[0]
-        const frameMesh:Mesh =  scene.children[0] as Mesh
-        (frameMesh.material as Material).dispose()
-        frameMesh.material = frameMaterial
-       
-        // engine.addObject(frameMesh)
-        frameMesh.position.y=45
-        frameMesh.rotation.y= Math.PI/180*-45
-        frameMesh.scale.set(2,2,2)
-        engine.addObject(frameMesh)
-        
-
+      getFrame().then(frameMesh=>{
+        frameMesh&& engine.addObject(frameMesh)
       })
     }, [])
     
--- src/views/learn-three/lib/load-model.ts
+++ src/views/learn-three/lib/load-model.ts
@@ -1,16 +1,32 @@
-import { MeshStandardMaterial, TextureLoader } from "three";
+import { Group, Material, Mesh, MeshStandardMaterial, TextureLoader } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import {frameColorTexture,frameDisplacementTexture,frameroughnessTexture} from './textures'
 
 const objLoader = new OBJLoader()
-const gltfLoader = new GLTFLoader()
-
-export const frameMaterial = new MeshStandardMaterial({
-  map:frameColorTexture,
-  roughnessMap:frameroughnessTexture,
-  bumpMap:frameDisplacementTexture
-})
+// const gltfLoader = new GLTFLoader()
 
 // export const framePromise = objLoader.loadAsync('/custom-model/frame.obj')
-export const framePromise = gltfLoader.loadAsync('/custom-model/frame.glb')
+
+export const getFrame = async ():Promise<Mesh|null>=>{
+  const frameMaterial = new MeshStandardMaterial({
+    map:frameColorTexture,
+    roughnessMap:frameroughnessTexture,
+    bumpMap:frameDisplacementTexture
+  })
+  const group = await objLoader.loadAsync('/custom-model/frame.obj')
+  if(group instanceof Group){
+    const frameMesh:Mesh =  group.children[0] as Mesh
+    (frameMesh.material as Material).dispose()
+    frameMesh.material = frameMaterial
+   
+    // engine.addObject(frameMesh)
+    frameMesh.position.y=45
+    frameMesh.rotation.y= Math.PI/180*-45
+    frameMesh.scale.set(2,2,2)
+    return frameMesh
+  }else{
+    console.error('group is not a Group');
+    return null
+  }
+}
