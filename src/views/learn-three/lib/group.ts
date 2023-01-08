050113af590b0e99794af1580607d1250ce7e2f7
liming.cao <liming.cao@rokid.com>
2023-01-08 15:19:28 +0800

    精灵图

--- src/views/learn-three/lib/group.ts
+++ src/views/learn-three/lib/group.ts
@@ -1,4 +1,4 @@
-import { Group, Mesh, MeshStandardMaterial, PlaneGeometry } from "three"
+import { Group, Mesh, MeshBasicMaterial, MeshStandardMaterial, PlaneGeometry, Sprite, SpriteMaterial } from "three"
 import { pictureTexture, tipsTexture } from "./textures"
 import { getFrame } from './load-model';
 
@@ -20,13 +20,27 @@ export const getGroup: () => Promise<Group> = async (): Promise<Group> => {
 
   group.add(picture)
 
-  const tips: Mesh = new Mesh(
-    new PlaneGeometry(19, 10),
-    new MeshStandardMaterial({
-      map: tipsTexture
+  // const tips: Mesh = new Mesh(
+  //   new PlaneGeometry(19, 10),
+  //   new MeshBasicMaterial({
+  //     map: tipsTexture
+  //   })
+  // )
+
+  const tips: Sprite = new Sprite(
+    new SpriteMaterial({
+      map: tipsTexture,
+      sizeAttenuation:false, // 物体不跟随镜头深度改变 而调整是镜头里的大小
+      depthWrite:false, //  depthWrite：false depthTest:false 不再计算是否被遮挡 始终显示在镜头里
+      depthTest:false
     })
   )
-  tips.position.set(0,-30,0)
+
+  tips.position.set(0,40,0)
+  tips.scale.set(0.16,0.09,1)
+  // tips.onBeforeRender = (...data)=>{
+  //   tips.lookAt(data[2].position)
+  // }
   
   group.add(tips)
   const frame = await getFrame()
